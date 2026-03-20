454e63b6460943803ac29b3c0db613f7fcc5adbb
WhosLogan <28313631+WhosLogan@users.noreply.github.com>
2026-03-19 22:06:57 -0400

    Set colors based on stores

--- src/components/Syrup.vue
+++ src/components/Syrup.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="syrup"></div>
+  <div class="syrup" :style="{ '--base-color': currentSyrup.color }"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {currentSyrup} from "../stores/beverage.ts";
+</script>
 <style lang="scss" scoped>
 .syrup {
   transform: translateY(400%);
-  background-color: #c6c6c6;
+  background-color: var(--base-color);
   position: relative;
   width: 100%;
   height: 20%;
